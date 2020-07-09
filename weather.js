5d2cdbb9fb6b75c76396e243ab703dbb18845a6b
Cooper Pearson <mptprogesterone@gmail.com>
2020-07-09 10:57:14 -0500

    fetch weather

--- weather.js
+++ weather.js
@@ -11,6 +11,13 @@ class Weather {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`)
 
         const responseData = await response.json();
+
         return responseData.current_observation;
+    }
+
+    //Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
     }
 }
