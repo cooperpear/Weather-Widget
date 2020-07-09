40a0bbbb566458d1e4d6658ae531d29bc90a0430
Cooper Pearson <mptprogesterone@gmail.com>
2020-07-09 11:27:46 -0500

    debug

---
+++ app.js
@@ -0,0 +1,14 @@
+//Initialize weather object
+const weather = new Weather('Austin', 'TX');
+
+//Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+function getWeather() {
+    weather.getWeather()
+        .then(results => {
+            console.log(results);
+        })
+        .catch(err => console.log(err));
+}
+
--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 class Weather {
     constructor(city, state) {
-        this.apiKey = "d06517398e8aaaadf62b3b9e18d611c1";
+        this.apiKey = "0e1250e2dc514ba28e33ffe53dd2011e";
         this.city = city;
         this.state = state;
 
@@ -8,11 +8,11 @@ class Weather {
 
     //Fetch Weather from API
     async getWeather() {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`)
 
         const responseData = await response.json();
 
-        return responseData.current_observation;
+        return responseData;
     }
 
     //Change weather location
